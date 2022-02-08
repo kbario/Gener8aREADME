f979a668ed1485a6b25de9edc7ba1c99ee88bdd4
kbario <kylebario1@gmail.com>
2022-02-08 12:28:06 +0800

    index.js now creates basic readme

--- index.js
+++ index.js
@@ -51,6 +51,16 @@ const questions = [
         name: 'contributors',
         message: 'Who else contributed to this'
     }, {
+        // TODO: contributions guidelines
+        type: 'input',
+        name: 'tutorials',
+        message: 'Did you follow any tutorials?'
+    }, {
+        // TODO: contributions guidelines
+        type: 'input',
+        name: 'thirdPartyAssets',
+        message: 'Did you use any third-party assets?'
+    }, {
         // TODO: test instructions
         type: 'input', 
         name: 'testIns',
@@ -87,73 +97,65 @@ const questions = [
 
 // TODO: Create a function to write README file
 function writeToFile({title, what, why, how, problems, learn, installation, usage, contributors, testIns, license, githubUsername, email}) {
+    // TODO for loop that creates md section of github users that contributed to the project
+
+    
     const readmeTemplate = `# ${title}
 
-    ## Description
-    
-    Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
-    
-    - What was your motivation?
-    - Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")
-    - What problem does it solve?
-    - What did you learn?
-    
-    ## Table of Contents (Optional)
-    
-    If your README is long, add a table of contents to make it easy for users to find what they need.
-    
-    - [Installation](#installation)
-    - [Usage](#usage)
-    - [Credits](#credits)
-    - [License](#license)
-    
-    ## Installation
-    
-    What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
-    
-    ## Usage
-    
-    Provide instructions and examples for use. Include screenshots as needed.
-    
-    To add a screenshot, create an \`assets/images\` folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
-    
-        \```md
-        ![alt text](assets/images/screenshot.png)
-        \```
-    
-    ## Credits
-    
-    List your collaborators, if any, with links to their GitHub profiles.
-    
-    If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
-    
-    If you followed tutorials, include links to those here as well.
-    
-    ## License
-    
-    The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
-    
-    ---
-    
-    🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
-    
-    ## Badges
-    
-    ![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
-    
-    Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
-    
-    ## Features
-    
-    If your project has a lot of features, list them here.
-    
-    ## How to Contribute
-    
-    If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
-    
-    ## Tests
-    
-    Go the extra mile and write tests for your application. Then provide examples on how to run them here.`
+## Description
+
+- ${what}
+- ${why}
+- ${how} 
+- ${problems}
+- ${learn}
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+
+${installation}
+
+## Usage
+
+${usage}
+
+## Credits
+
+// TODO colaborators from for loop above
+
+// TODO third party assets
+
+// TODO tutorials
+
+## License
+
+${license}
+
+## Badges
+
+![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
+
+Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
+
+## Features
+
+If your project has a lot of features, list them here.
+
+## How to Contribute
+
+If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
+
+## Tests
+
+Go the extra mile and write tests for your application. Then provide examples on how to run them here.`
+
+    fs.writeFile('./README.md', readmeTemplate, (err) => err ? console.log(err) : console.log("Success!"))
 }
 
 // TODO: Create a function to initialize app
@@ -161,7 +163,7 @@ function init() {
     inquirer
     .prompt(questions)
     .then((answers) => {
-        writeToFile(answers, answers)
+        writeToFile(answers)
     });
 }
 
