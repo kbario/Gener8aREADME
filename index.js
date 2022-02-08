0dee6cec765ce3f3e566ecb8c7c6dcc210599359
kbario <kylebario1@gmail.com>
2022-02-08 10:10:21 +0800

    initial setup

---
+++ index.js
@@ -0,0 +1,36 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+        // TODO: title fo the project
+    }, {
+        // TODO: description
+    }, {
+        // TODO: installation instructions
+    }, {
+        // TODO: usage information
+    }, {
+        // TODO: contributions guidelines
+    }, {
+        // TODO: test instructions
+    }, {
+        // TODO: licence
+        // TODO: list of options
+    }, {
+        // TODO: github username    
+    }, {
+        // TODO: email
+    },
+];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
